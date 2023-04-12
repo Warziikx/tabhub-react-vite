a71d28c141e25b761f25bd6bf8a27c4b8a569bfd
Durand Nicolas <nicolas.durand@sotiaf.fr>
2023-04-12 08:38:38 +0200

    :sparkles: Bokkmark edit load button

--- src/components/bookmark/form/BookmarkDrawerForm.tsx
+++ src/components/bookmark/form/BookmarkDrawerForm.tsx
@@ -1,43 +1,55 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button, Drawer, Form, Space } from "antd";
 
 import { BookmarkForm } from "@/components/bookmark/form/BookmarkForm";
 import useCollectionContext from "@/lib/context/CollectionContext";
 
 export const BookmarkDrawerForm: React.FC = () => {
-    const { isDrawerBookmarkFormOpen, setIsDrawerBookmarkFormOpen, currentContextBookmark } = useCollectionContext();
-    const [form] = Form.useForm();
+	const [isLoading, setIsLoading] = useState(false);
+	const { isDrawerBookmarkFormOpen, setIsDrawerBookmarkFormOpen, currentContextBookmark } = useCollectionContext();
+	const [form] = Form.useForm();
 
+	useEffect(() => {
+		if (currentContextBookmark != null) {
+			form.setFieldsValue(currentContextBookmark);
+			form.setFieldValue("imagePath", [
+				{ uid: currentContextBookmark.imagePath, name: currentContextBookmark.imagePath, percent: 100, thumbUrl: currentContextBookmark.imageLink, type: "image/jpeg" },
+			]);
+		}
+	}, [form, currentContextBookmark]);
 
-    useEffect(() => {
-        if (currentContextBookmark != null) {
-            form.setFieldsValue(currentContextBookmark);
-            form.setFieldValue('imagePath', [{ uid: currentContextBookmark.imagePath, name: currentContextBookmark.imagePath, percent: 100, thumbUrl: currentContextBookmark.imageLink, type: "image/jpeg" }])
-        }
+	const onButtonClick = () => {
+		setIsLoading(true);
+		form.submit();
+	};
 
-    }, [form, currentContextBookmark]);
+	const submitCallback = () => {
+		setIsLoading(false);
+		form.resetFields();
+		setIsDrawerBookmarkFormOpen(false);
+	};
 
-    const onSubmit = () => {
-        form.resetFields();
-        setIsDrawerBookmarkFormOpen(false);
-    };
+	const handleCancel = () => {
+		//TODO: Loading Effect on Button
+		form.resetFields();
+		setIsDrawerBookmarkFormOpen(false);
+	};
 
-    const handleCancel = () => {
-        //TODO: Loading Effect on Button
-        form.resetFields();
-        setIsDrawerBookmarkFormOpen(false);
-    };
-
-    return (
-        <Drawer title={"Editer"} open={isDrawerBookmarkFormOpen} onClose={handleCancel} extra={
-            <Space>
-                <Button onClick={handleCancel}>Annuler</Button>
-                <Button onClick={form.submit} type="primary">
-                    Modifier
-                </Button>
-            </Space>
-        }>
-            <BookmarkForm form={form} submitCallback={onSubmit} />
-        </Drawer>
-    );
-}
+	return (
+		<Drawer
+			title={"Editer"}
+			open={isDrawerBookmarkFormOpen}
+			onClose={handleCancel}
+			extra={
+				<Space>
+					<Button onClick={handleCancel}>Annuler</Button>
+					<Button onClick={onButtonClick} type="primary" loading={isLoading} disabled={isLoading}>
+						Modifier
+					</Button>
+				</Space>
+			}
+		>
+			<BookmarkForm form={form} submitCallback={submitCallback} />
+		</Drawer>
+	);
+};
